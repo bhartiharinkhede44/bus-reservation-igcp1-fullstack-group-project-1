76e2d454aa77149d32c4886620c9644fdc75fefa
Harshali Kanoje <harshalikanoje@gmail.com>
2023-12-03 02:07:24 -0800

    signup api is ready

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
+import User from "./src/Model/User.js";
 
 
 const app = express();
@@ -16,8 +17,38 @@ const connectDB = async () => {
 }
 connectDB();
 
+app.post('/signup', async (req, res) => {
+    const { name, email, password, mobile, address, gender } = req.body
+
+    const user = new User({
+        name: name,
+        email: email,
+        password: password,
+        mobile: mobile,
+        address: address,
+        gender: gender
+    })
+
+    try {
+        const saveduser = await user.save();
+
+        res.json({
+            success: true,
+            data: saveduser,
+            message: "fetch studendt"
+        })
+    }
+
+    catch (e) {
+        res.json({
+            success: false,
+            message: e.message
+        })
+    }
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
-   })
+})
