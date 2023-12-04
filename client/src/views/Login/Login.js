dae2c5a029e676816750e6ff8ac032e7449f8430
Harshali Kanoje <harshalikanoje@gmail.com>
2023-12-04 00:14:51 -0800

    styling given to signup form

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React ,{useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
 
@@ -10,72 +10,76 @@ const Login = () => {
   const [password, setPassword] = useState('')
 
   const login = async () => {
-     const response = await axios.post('/login' , {
-      email:email,
-      password:password
-     })
+    const response = await axios.post('/login', {
+      email: email,
+      password: password
+    })
 
-     alert(response?.data?.message)
+    alert(response?.data?.message)
 
-    if(response?.data?.success)
-    {
-      localStorage.setItem("user",JSON.stringify(response?.data?.data));
+    if (response?.data?.success) {
+      localStorage.setItem("user", JSON.stringify(response?.data?.data));
       window.location.href = '/';
     }
   }
 
-  const [user , setUser] = useState({})
+  const [user, setUser] = useState({})
 
-    useEffect(() => {
-        const storeuser = JSON.parse(localStorage.getItem("user" || '{}'))
-        if(storeuser?.email)
-        {
-          alert("You have already logged in!")
-          window.location.href = '/';
-        }
-    },[])
+  useEffect(() => {
+    const storeuser = JSON.parse(localStorage.getItem("user" || '{}'))
+    if (storeuser?.email) {
+      alert("You have already logged in!")
+      window.location.href = '/';
+    }
+  }, [])
 
   return (
-    <div>
+    <div className='login-body'>
 
-    <div className='j-content-center'>
-    <form className='form-container'>
-      <h1 className='text-center'>Login</h1>
+      <div className='j-content-center '>
 
-      <div className='form-input'>
-        <label htmlFor='email'>Email : </label>
-        <br/>
-        <input  
-        type='text'
-        placeholder='Enter Your Email'
-          id='email'
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value)
-          }} />
+        <form className='form-container'>
+          <h1 className='text-center login-head'>Login Here</h1>
+
+          <div className='form-input'>
+            <label htmlFor='email' className=' label'>Email  </label>
+            <br />
+            <input
+              type='text'
+              placeholder='Enter Your Email'
+              id='email'
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }}
+              className='input'
+            />
+          </div>
+
+          <div className='form-input'>
+            <label htmlFor='password' className=' label'>Password  </label>
+            <br />
+            <input
+              placeholder='Enter Your Password'
+              type='password'
+              id='password'
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value)
+              }}
+              className='input'
+            />
+          </div>
+          <div className='login-btn-container'>
+            <button type='button' className='login-btn' onClick={login}>Login</button>
+            <p>
+              <Link to={'/signup'} className='create-account'>Create An Account</Link>
+            </p>
+          </div>
+
+        </form>
       </div>
-
-      <div className='form-input'>
-        <label htmlFor='password'>Password : </label>
-        <br/>
-        <input 
-        placeholder='Enter Your Password'
-        type='password'
-          id='password'
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value)
-          }}/>
-      </div>
-
-      <button type='button' className='signup-btn' onClick={login}>Login</button>
-
-      <p>
-        <Link to={'/signup'}>Create An Account</Link>
-      </p>
-    </form>
-  </div>
-  </div>
+    </div>
   )
 }
 
