afa67528bdcd01511a05fd7af489835f39e97d88
kashishInamdar <kashishinamdar2004@gmail.com>
2023-12-07 22:17:47 +0530

    cteate boge page & add bloge API

--- client/src/views/Blog/Blog.js
+++ client/src/views/Blog/Blog.js
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react';
 import './Blog.css';
-import axios from "axios"
-
+import Navbar from "./../../components/Navbar/Navbar.js"
+import axios from "axios" ;
 
 function Blog() {
+
+    const [blogs , setBlogs] = useState([])
+
+
+const getBlog = async ()=>{
+    const respons = await axios.get("/api/v1/blogs")
+    setBlogs(respons?.data?.data)
+}
+
+
+useEffect(()=>{
+    getBlog()
+},[])
+
     return<>
-    <div>
-    Blog
+    <div className='Blog-page' >
+        <Navbar />
+        <div>
+            <div className='fash-page'>
+                <h1 className='title'> Bus Reservation </h1>
+                <span className='quote'>Life is a journey that must be traveled no matter how bad the roads and accommodations.</span>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+    
     </div>
     </>
 }
